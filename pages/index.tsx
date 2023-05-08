c6003257cad804607052a20f7dada8ae03d9a82c
Joe <joe@trlab.com>
2023-05-08 17:13:01 +0800

    feat: reduce info

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,21 +23,12 @@ export default function Home() {
           <p>lp + position value: {commifyValue(hedgeInfo.userLpValue.add(hedgeInfo.totalPositionValue))}</p>
           {hedgeInfo.tokenInfos.map((tokenInfo, i) => (
             <Card title={tokenInfo.name} bordered={true} style={{width: 1000, background: 'gray'}} key={tokenInfo.name}>
-              <p> price: {commifyPrice(tokenInfo.asset.price)} </p>
-              <p> pool amount {commifyUnits(tokenInfo.asset.effectivePoolAmount)} </p>
-              <p> user pool effective amount {commifyUnits(tokenInfo.userEffectivePoolAmount)} </p>
               <p> user pool effective value {commifyValue(tokenInfo.userEffectivePoolValue)}</p>
               <p> hedge position size: {commifyValue(tokenInfo.position.size)}</p>
-              <p> hedge position effective leverage: {tokenInfo.position.size.div(tokenInfo.position.collateral).toString()}</p>
+              <p> price: {commifyPrice(tokenInfo.asset.price)} </p>
               <p> hedge position averagePrice: {commifyValue(tokenInfo.position.averagePrice)}</p>
-              <p> hedge position collateral: {commifyValue(tokenInfo.position.collateral)}</p>
-              <p> hedge position net collateral: {commifyValue(tokenInfo.position.netCollateral)}</p>
-              <p> hedge position pnl: {commifyValue(tokenInfo.position.pnl)}</p>
               <p> hedge position net pnl: {commifyValue(tokenInfo.position.netPnl)}</p>
-              <p> hedge position net value: {commifyValue(tokenInfo.position.netValue)}</p>
               <p> hedge position funding fee: {commifyValue(tokenInfo.position.fundingFee.mul(-1))}</p>
-              <p> hedge position funding fee debt: {commifyValue(tokenInfo.position.fundingFeeDebt.mul(-1))}</p>
-              <p> hedge position borrowing fee: {commifyValue(tokenInfo.position.borrowingFee)}</p>
               {/*<p> hedge position delta: {formatPositionDelta(tokenInfo.positionDelta)}</p>*/}
             </Card>
           ))}
